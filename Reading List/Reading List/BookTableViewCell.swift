030e710ee2c0bfb323e4a52d2997997b117d2e14
Dongwoo Pae <dongwoopae@gmail.com>
2019-05-11 09:21:45 -0700

    set up BookTableViewCell and protocol

--- Reading List/Reading List/BookTableViewCell.swift
+++ Reading List/Reading List/BookTableViewCell.swift
@@ -6,11 +6,22 @@
 //  Copyright © 2019 Lambda School. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
 class BookTableViewCell: UITableViewCell {
     @IBOutlet weak var bookTitleLabel: UILabel!
     @IBOutlet weak var checkButton: UIButton!
+    
+    
+    var book: Book? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    var delegate : BookTableViewCellDelegate?
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -24,6 +35,19 @@ class BookTableViewCell: UITableViewCell {
     }
 
     @IBAction func checkButtonTapped(_ sender: Any) {
+        self.delegate?.toggleHasbeenRead(for: self)
+    }
+    
+    
+    func updateViews() {
+        guard let bookTitle = book?.title,
+            let bookHasBeeRead = book?.hasBeenRead else {return}
+        bookTitleLabel.text = bookTitle
         
+        if bookHasBeeRead {
+            checkButton.setImage(UIImage(named:"checked"), for: .normal)
+        } else {
+            checkButton.setImage(UIImage(named: "unchecked"), for: .normal)
+        }
     }
 }
